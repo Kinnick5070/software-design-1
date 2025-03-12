7515eeacf9f54e6532480ad65f0590f8fcf310e2
Jonah Faas <faasj@students.westerntc.edu>
2025-03-12 09:05:14 -0500

    talked about polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,13 @@
+﻿List<Airplane> planeList = new List<Airplane>();
+
+Airplane airplaneOne = new Airplane("Delta", 35000, 2);
+
+Automobile carOne = new Automobile("AB123", 5, true);
+
+Vehicle vehicleOne = new Vehicle();
+
+planeList.Add(airplaneOne);
+
+foreach(Airplane plane in planeList){
+    Console.WriteLine($"{plane.Airline}");
+}
